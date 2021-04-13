200edbcee10500419a50ed67c5a891a6d3e3d340
Aukarin <52068994+Aukarin@users.noreply.github.com>
2021-04-13 13:17:12 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-   
-    mongoUri:  "mongodb+srv://admin:Auk0989472168@cluster0.flhzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-     PORT: process.env.PORT || 3000,
-}
